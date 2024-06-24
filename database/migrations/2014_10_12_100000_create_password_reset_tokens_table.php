32e60bc8b99c90015aef384978e2d73425e331bf
kutta6969 <167262115+kutta6969@users.noreply.github.com>
2024-06-24 09:39:15 -0600

    1

--- database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
+++ database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
@@ -1,15 +1,15 @@
 <?php
-
+ 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
+ 
 return new class extends Migration
 {
     /**
      * Run the migrations.
      */
-    public function up()
+    public function up(): void
     {
         if (!Schema::hasTable('password_reset_tokens')) {
             Schema::create('password_reset_tokens', function (Blueprint $table) {
@@ -19,7 +19,7 @@ return new class extends Migration
             });
         }
     }
-
+ 
     /**
      * Reverse the migrations.
      */
